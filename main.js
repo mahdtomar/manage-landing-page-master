ba7566b940e14ad5793563d57115e924ceb822e4
Omar Mahdy <72965518+mahdtomar@users.noreply.github.com>
2022-10-06 13:09:50 +0200

    mobile drop menu

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+const menuIcon = document.getElementById("hamburgerIcon");
+const navigationMenu = document.getElementById("ul");
+const hamburgerSrc = "./images/icon-hamburger.svg";
+const closeSrc = "./images/icon-close.svg";
+
+// managing the navigation menu state ;
+menuIcon.addEventListener("click", () => {
+  if (menuIcon.attributes.src.nodeValue == hamburgerSrc) {
+    menuIcon.attributes.src.nodeValue = closeSrc;
+    navigationMenu.style.display = "block";
+  } else {
+    menuIcon.attributes.src.nodeValue = hamburgerSrc;
+    navigationMenu.style.display = "none";
+  }
+});
